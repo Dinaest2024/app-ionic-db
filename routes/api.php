cabe3a7e7736b0885bf007877c6f76706f775408
Mitzy <mitzy.moreno@masabi.com>
2025-03-28 21:16:39 -0500

    apis

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GiftController;
+
+//Route::get('/user', function (Request $request) {
+  //  return $request->user();
+//})->middleware('auth:sanctum');
+
+//Route::get('create_gift', [GiftController::class, 'store'])
+Route::resourcel('/gift', [GiftController::class])
